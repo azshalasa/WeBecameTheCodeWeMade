d05d4b3567bb3dfd2b0c5c321e8fcf93039ce1a3
Azshalasa <mts.c@live.com>
2018-09-02 14:44:58 -0300

    Update lib.h

--- Agenda/lib.h
+++ Agenda/lib.h
@@ -4,6 +4,6 @@ struct Agenda{
 };
 
 void menu();
-void refreshVar(void *pBuffer, void **op, void **count, void **lp, struct Agenda **FirstC, struct Agenda **LastC);
-void AddTo(void *pBuffer, int *count);
-void PrintAll(void *pBuffer, struct Agenda **FirstC, struct Agenda **LastC);
+void refreshVar(void *pBuffer, void **op, void **count, void **lp);
+void *AddTo(void *pBuffer, int *count);
+void PrintAll(void *pBuffer,int *count, int *lp);
