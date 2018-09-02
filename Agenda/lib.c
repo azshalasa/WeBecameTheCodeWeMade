af8e3053abdc312864cfa3eacc97bcacc9a7b152
Azshalasa <mts.c@live.com>
2018-09-01 22:05:56 -0300

    Update lib.c

--- Agenda/lib.c
+++ Agenda/lib.c
@@ -2,43 +2,53 @@
 #include <stdlib.h>
 #include "lib.h"
 
-void refreshVar(void *pBuffer, void **op, void **count, void **lp){
+void refreshVar(void *pBuffer, void **op, void **count, void **lp, struct Agenda **FirstC, struct Agenda **LastC){
     *op = pBuffer;
     *lp = pBuffer+sizeof(int);
     *count = pBuffer + (2*sizeof(int));
 
-    printf("OP : %i\n", op);
-    printf("CO : %i\n", count);
-    printf("LP : %i\n", lp);
-
+    if(*(int *)(pBuffer + (2*sizeof(int))) > 0){ // Verify if count > 0
+        *FirstC = pBuffer + (2*sizeof(int)) + (sizeof(struct Agenda)); // If count > 0 we should refresh the pointer to contact list
+        *LastC = pBuffer + (2*sizeof(int)) + ( *(int *)(pBuffer + (2*sizeof(int))) * sizeof(struct Agenda));
+    }else{
+        *FirstC = NULL;
+        *LastC = NULL;
+    }
 }
-/*
-void AddTo(void *pBuffer){
-        pBuffer = realloc(pBuffer, (sizeof(pBuffer) + sizeof(struct Agenda)));
+
+void AddTo(void *pBuffer, int *count){
+
+        *count = *count + 1;
+
+        pBuffer = realloc(pBuffer, (3 * sizeof(int)) + ( (*count) * sizeof(struct Agenda)));
         struct Agenda * test;
-        test = pBuffer + (sizeof(int)*3) + sizeof(struct Agenda);
+
+        test = pBuffer + (3 * sizeof(int)) + ( (*count) * sizeof(struct Agenda));
+
         printf("Digite o Numero\n");
         fflush(stdin);
         scanf("%d", &test->cphone);
         printf("Digite o Nome\n");
         fflush(stdin);
         fgets (test->name , (sizeof(char)*10) , stdin);
-        *(int *)(pBuffer + (2*sizeof(int))) = *(int *)(pBuffer + (2*sizeof(int))) +1;
 }
 
-void PrintAll(void *pBuffer){
+void PrintAll(void *pBuffer, struct Agenda **FirstC, struct Agenda **LastC){
+    printf("P1 %d\n", *FirstC);
+    printf("P2 %d\n", *LastC);
+
     if(*(int *)(pBuffer + (2*sizeof(int)))>0){
         struct Agenda * test;
-        for(*(int *)(pBuffer + sizeof(int)) = 0; *(int *)(pBuffer + sizeof(int)) < *(int *)(pBuffer + (2*sizeof(int))); (*(int *)(pBuffer + sizeof(int)))++){
+        for(*FirstC = *FirstC; *FirstC < *LastC;){
             test = pBuffer + (sizeof(int)*3) + sizeof(struct Agenda) * (*(int *)(pBuffer + (2*sizeof(int))));
             printf("\n");
-            printf("%d\n", test->cphone);
-            printf("%s\n", test->name);
+            printf("Phone:%d\n", test->cphone);
+            printf("Name:%s\n", test->name);
             printf("\n");
         }
     }
 }
-*/
+
 void menu(){
         /*pBuffer = realloc(pBuffer, (sizeof(pBuffer) + sizeof(struct Agenda)));
         struct Agenda * test;
