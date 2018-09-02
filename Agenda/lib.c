6c706e7855425b669a2b16a6f6440fab4b500bc4
Azshalasa <mts.c@live.com>
2018-09-02 15:01:50 -0300

    Update lib.c

--- Agenda/lib.c
+++ Agenda/lib.c
@@ -1,21 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "lib.h"
-
+// refresVar
+// pBuffer  = Ponteiro tipo void para o buffer
+// *count   = Ponteiro para ponteiro tipo void
+// *lp      = Ponteiro para ponteiro tipo void
+// *op      = Ponteiro para ponteiro tipo void
 void refreshVar(void *pBuffer, void **op, void **count, void **lp){
     *op = pBuffer;                              // Atualiza ponteiro do menu de opções
     *lp = pBuffer + sizeof(int);                // Atualiza ponteiro de looping
     *count = pBuffer + (2*sizeof(int));         // Atualiza ponteiro de count
 }
-
+// AddTo    = função tipo ponteiro para void, deve retornar ponteiro para pBuffer
+// pBuffer  = Ponteiro tipo void para o buffer
+// *count   = Ponteiro tipo int para variavel de contagem
+// *lp      = Ponteiro tipo int para variavel de looping
 void *AddTo(void *pBuffer, int *count){
-        //printf("-----------------\n");
-        //printf("BRealloc: %d \n", pBuffer);
+        printf("-----------------\n");
+        printf("BRealloc: %d \n", pBuffer);
         *count = *count + 1;
         pBuffer = (void*)realloc(pBuffer, (3 * sizeof(int)) + ( (*count) * sizeof(struct Agenda)));
-        //printf("--------------%d\n", (3 * sizeof(int)) + ( (*count) * sizeof(struct Agenda)));
-        //printf("-----------------\n");
-        //printf("pBuffer: %d Count: %d \n", pBuffer, *count);
+        printf("--------------%d\n", (3 * sizeof(int)) + ( (*count) * sizeof(struct Agenda)));
+        printf("-----------------\n");
+        printf("pBuffer: %d Count: %d \n", pBuffer, *count);
 
         struct Agenda * test;
         test = pBuffer + (3 * sizeof(int)) + ( (*count) * sizeof(struct Agenda));
@@ -29,7 +36,10 @@ void *AddTo(void *pBuffer, int *count){
         fgets (test->name , (sizeof(char)*10) , stdin);
         return pBuffer;
 }
-
+// PrintAll
+// pBuffer  = Ponteiro tipo void para o buffer
+// *count   = Ponteiro tipo int para variavel de contagem
+// *lp      = Ponteiro tipo int para variavel de looping
 void PrintAll(void *pBuffer, int *count, int *lp){
     if(*count > 0){
         struct Agenda * test;
