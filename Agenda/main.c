46e09fc1f30feb662a394c3d827837fdcc576599
Azshalasa <mts.c@live.com>
2018-09-03 16:24:16 -0300

    Update main.c

--- Agenda/main.c
+++ Agenda/main.c
@@ -16,18 +16,16 @@ int main()
     *(int *)(pBuffer + (2*sizeof(int))) = 1;                // Variavel para Contar, inicializa em 1 pois o primeiro contato serve para gerenciamento interno do programa
     refreshVar(pBuffer, &op, &count, &lp);                  // Função que atualiza os ponteiros locais de main
     Note = pBuffer + (3 * sizeof(int)) + ( (*(int *)count) * sizeof(struct Agenda));
-    Note->cphone = 0;
+    Note->cphone = 99;
     Note->name[0] = 'x';
-    printf("Buffer: %d\n", pBuffer + (3 * sizeof(int)) + ( (*(int *)count) * sizeof(struct Agenda)));
 
-    //pBuffer = AddTo(pBuffer, count);
-    //refreshVar(pBuffer, &op, &count, &lp);
-    //pBuffer = AddTo(pBuffer, count);
+    pBuffer = AddTo(pBuffer, count);
     refreshVar(pBuffer, &op, &count, &lp);
-    //pBuffer = AddTo(pBuffer, count);
-    //refreshVar(pBuffer, &op, &count, &lp);
+    pBuffer = RemoveTo(pBuffer, count, lp);
+    refreshVar(pBuffer, &op, &count, &lp);
 
     PrintAll(pBuffer, count, lp);
+
 
     return 0;
 }
