f1a1ae35d673b385e26fa8ae49d215f768ae917e
Azshalasa <mts.c@live.com>
2018-09-03 12:13:27 -0300

    Update main.c

--- Agenda/main.c
+++ Agenda/main.c
@@ -8,18 +8,26 @@ int main()
     void *op;
     void *count;
     void *lp;
+    struct Agenda * Note;
 
-    pBuffer = (void*)malloc((sizeof(int)*3));               // Aloca os 3 inteiros descritos abaixo no inicio do programa
+    pBuffer = (void*)malloc((sizeof(int)*3 + sizeof(struct Agenda)));               // Aloca os 3 inteiros descritos abaixo no inicio do programa
     *(int *)pBuffer = 0;                                    // Variavel para Menu de Opção
     *(int *)(pBuffer + sizeof(int)) = 0;                    // Variavel para Looping
-    *(int *)(pBuffer + (2*sizeof(int))) = 0;                // Variavel para Contar
+    *(int *)(pBuffer + (2*sizeof(int))) = 1;                // Variavel para Contar, inicializa em 1 pois o primeiro contato serve para gerenciamento interno do programa
     refreshVar(pBuffer, &op, &count, &lp);                  // Função que atualiza os ponteiros locais de main
+    Note = pBuffer + (3 * sizeof(int)) + ( (*(int *)count) * sizeof(struct Agenda));
+    Note->cphone = 0;
+    Note->name[0] = 'x';
+    printf("Buffer: %d\n", pBuffer + (3 * sizeof(int)) + ( (*(int *)count) * sizeof(struct Agenda)));
 
-    pBuffer = AddTo(pBuffer, count);
+    //pBuffer = AddTo(pBuffer, count);
+    //refreshVar(pBuffer, &op, &count, &lp);
+    //pBuffer = AddTo(pBuffer, count);
     refreshVar(pBuffer, &op, &count, &lp);
+    //pBuffer = AddTo(pBuffer, count);
+    //refreshVar(pBuffer, &op, &count, &lp);
+
     PrintAll(pBuffer, count, lp);
-    pBuffer = AddTo(pBuffer, count);
-    refreshVar(pBuffer, &op, &count, &lp);
 
     return 0;
 }
