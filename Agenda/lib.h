dc85a0ceddd1621ecaaf6cddca10253c0f1f41c8
Azshalasa <mts.c@live.com>
2018-10-01 20:53:50 -0300

    Update lib.h
    Sorts Adicionados

--- Agenda/lib.h
+++ Agenda/lib.h
@@ -9,3 +9,8 @@ void *AddTo(void *pBuffer, void *count);
 void *RemoveTo(void *pBuffer, void *count, void *lp, void *op);
 void SearchTo(void *pBuffer, void *count, void *lp, void *op);
 void PrintAll(void *pBuffer, void *count, void *lp);
+// Sorts of Sorts
+void Bubble(void *pBuffer, void *count, void *lp, void *op);
+void Insertion(void *pBuffer, void *count, void *lp, void *op);
+void Selection(void *pBuffer, void *count, void *lp, void *op);
+void Random(void *pBuffer, void *count, void *lp, void *op);
